1ee8923364f2a109fde77ba092469823ebc16a0e
Anumi Thevhara <anumithevhara@gmail.com>
2022-02-01 17:13:43 +0530

    first

--- app.py
+++ app.py
@@ -5,7 +5,10 @@ st.sidebar.selectbox('Options', options)
 st.title('Hello Anumi')
 st.write('This is my first web application')
 
-video_file = open('Lemon - 82602.mp4')
-video_bytes = video_file.read()
 
-st.video(video_bytes)
+# video_file = open('Lemon - 82602.mp4')
+# video_bytes = video_file.read()
+
+# st.video(video_bytes)
+
+st.video('https://www.youtube.com/watch?v=D9tTi-CDjDU')
