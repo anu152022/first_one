a35a60e766292bb2ac0db0dca974cb48403c5c39
anu152022 <98630867+anu152022@users.noreply.github.com>
2022-02-02 12:33:21 +0530

    streamlit run app.py

--- app.py
+++ app.py
@@ -8,15 +8,15 @@ st.write('Welcome to my Covid Dashboard')
 st.write('Enjoy it well!!')
 days = st.sidebar.slider('Pick your number of music', min_value=1,max_value=90)
 
-video_file = open('Lemon - 82602.mp4')
-video_bytes = video_file.read()
+# video_file = open('Lemon - 82602.mp4')
+# video_bytes = video_file.read()
 
-st.video(video_bytes)
+# st.video(video_bytes)
 
 page_bg_img = '''
 <style>
 body {
-background-image: url("https://media.istockphoto.com/photos/coronavirus-virus-outbreak-picture-id1206774789?k=20&m=1206774789&s=612x612&w=0&h=7MgIbLQbVUCSic3wnpcqAj-0fNZdD_s-w5QTVxO_MNU=");
+background-image: url("https://media.istockphoto.com/photos/blue-picture-id1213090148?k=20&m=1213090148&s=170667a&w=0&h=OheD3nObHHYhgIQw30xo-m1mDseWmkshgPiM-sgNgjE=");
 background-size: cover;
 }
 </style>
