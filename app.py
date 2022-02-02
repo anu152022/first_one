0a75525aaa3176aab9e512d46500657ff54c5987
anu152022 <98630867+anu152022@users.noreply.github.com>
2022-02-02 12:08:49 +0530

    streamlit run app.py

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ import streamlit as st
 
 options =  ['Vlog no copyright', 'Background music']
 st.sidebar.selectbox('Options', options)
-st.title('Hello Anumi')
-st.write('These are my favourite musics')
+st.title('My Covid Dashboard')
+st.write('Welcome to my Covid Dashboard')
+st.write('Enjoy it well!!')
 days = st.sidebar.slider('Pick your number of music', min_value=1,max_value=90)
 # data_type = st.sidebar.multiselect('Pick data types', data_types)
 
