fd5ed103e4bc550a08f41718255f207787cb64ad
anu152022 <98630867+anu152022@users.noreply.github.com>
2022-02-02 10:19:59 +0530

    streamlit run app.py

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import streamlit as st
 
+
 options =  ['Vlog no copyright', 'Background music']
 st.sidebar.selectbox('Options', options)
 st.title('Hello Anumi')
 st.write('These are my favourite musics')
+days = st.sidebar.slider('Pick your days', min_value=1,max_value=90)
+data_type = st.sidebar.multiselect('Pick data types', data_types)
 
 
 # video_file = open('Lemon - 82602.mp4')
