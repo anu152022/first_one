ab77258c570103216951d2893ca36bd990205b79
anu152022 <98630867+anu152022@users.noreply.github.com>
2022-02-02 12:36:30 +0530

    sttreamlit run app.py

--- app.py
+++ app.py
@@ -6,20 +6,23 @@ st.sidebar.selectbox('Options', options)
 st.title('My Covid Dashboard')
 st.write('Welcome to my Covid Dashboard')
 st.write('Enjoy it well!!')
-days = st.sidebar.slider('Pick your number of music', min_value=1,max_value=90)
+days = st.sidebar.slider('Pick your number of music', min_value=1,max_value=50)
 
 # video_file = open('Lemon - 82602.mp4')
 # video_bytes = video_file.read()
 
 # st.video(video_bytes)
 
-page_bg_img = '''
-<style>
-body {
-background-image: url("https://media.istockphoto.com/photos/blue-picture-id1213090148?k=20&m=1213090148&s=170667a&w=0&h=OheD3nObHHYhgIQw30xo-m1mDseWmkshgPiM-sgNgjE=");
-background-size: cover;
-}
-</style>
-'''
-
-st.markdown(page_bg_img, unsafe_allow_html=True)
+st.markdown(
+    """
+    <style>
+    .reportview-container {
+        background: url("https://images.app.goo.gl/LFCobouKtT7oZ7Qv7")
+    }
+   .sidebar .sidebar-content {
+        background: url("https://images.app.goo.gl/LFCobouKtT7oZ7Qv7")
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
